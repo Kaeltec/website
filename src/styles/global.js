22ad425d07cfb2ae8be653f72fae8be0951a5808
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-05-31 21:48:19 -0300

    wip: scrool reworked

--- src/styles/global.js
+++ src/styles/global.js
@@ -9,20 +9,20 @@ export default createGlobalStyle`
     font: 1.6rem 'Righteous', cursive;
     color: #fff;
     transition: font-size 200ms ease-in-out;
-    background-color: ${props => props.theme.dark_solid};
+    background-color: ${props => props.theme.dark};
   }
 
   body,
   &.scroll {
     &::-webkit-scrollbar-thumb {
       background-color: ${props => props.theme.grey};
-      border-radius: 6px;
     }
 
     &::-webkit-scrollbar {
       width: 8px;
       margin: 3px;
       padding: 10px;
+      background-color: ${props => props.theme.grey_solid};
     }
   }
 
