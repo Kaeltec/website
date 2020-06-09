fbb9ed707028382e16b7254d67033da19cbb6736
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-06-08 23:58:58 -0300

    feat: offline plugin

--- next.config.js
+++ next.config.js
@@ -2,15 +2,21 @@ const bundleAnalyzer = require('@next/bundle-analyzer');
 const sourceMaps = require('@zeit/next-source-maps');
 const composePlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const withPWA = require('next-pwa');
 
 module.exports = composePlugins(
   [
-    [optimizedImages],
     [sourceMaps],
+    [optimizedImages],
+    [bundleAnalyzer, { enabled: !!process.env.ANALYZE }],
     [
-      bundleAnalyzer,
+      withPWA,
       {
-        enabled: !!process.env.ANALYZE,
+        pwa: {
+          dest: 'public',
+          register: true,
+          disable: process.env.NODE_ENV !== 'production',
+        },
       },
     ],
   ],
